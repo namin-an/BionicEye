3ca896c117a89eefa2df9e0dbd77703640cfad49
namin-an <namin0202@gmail.com>
2022-10-25 01:16:38 +0900

    Extend dataframe formats

--- code/main.py
+++ code/main.py
@@ -18,7 +18,7 @@ def main(cfg: DictConfig):
     print(f"The original cwd: {orig_cwd}")
 
     # Configuration check
-    assert cfg.environment.name in ['HumBehTask', 'CartPole-v1'], "Invalid environment!"
+    assert cfg.environment.name in ['Bioniceye', 'CartPole-v1'], "Invalid environment!"
     assert cfg.algorithm.name in ['PPO', 'REINFORCE', 'AC', 'DQN'], "Invalid algorithm!"
 
     # General setup
@@ -29,37 +29,44 @@ def main(cfg: DictConfig):
     image_dir = f"{orig_cwd}\{cfg.directory.image_dir}"
     label_path = f"{orig_cwd}\{cfg.directory.label_path}"
     pred_dir = f"{orig_cwd}\{cfg.directory.pred_dir}"
-    stim_type = cfg.type.stim_type
-    top1 = cfg.top1
+    stim_type = cfg.data.stim_type
+    top1 = cfg.data.top1
 
     data_path = f"{orig_cwd}\{cfg.directory.data_path}"
-    class_num = cfg.class_num
+    class_num = cfg.data.class_num
 
-    check_time_usage = cfg.check_time_usage
     env_type = cfg.environment.name
-    model_type = cfg.type.model_type
+    model_type = cfg.algorithm.name
     episode_num = cfg.training.episode_num
     print_interval = cfg.training.print_interval
     learning_rate = cfg.reinforcement.learning_rate
-    discount = cfg.reinforcement.discount
-    average_window = cfg.evaluation.average_window
+    gamma = cfg.reinforcement.gamma
+    batch_size = cfg.training.batch_size
+    render = cfg.training.render
+
+    # Algorithm specificity
+    if model_type == 'PPO':
+        lmbda = cfg.algorithm.lmbda
+        eps_clip = cfg.algorithm.eps_clip
+        argv = [lmbda, eps_clip]
+    else:
+        argv = []
 
     # GPU check
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print(f"Current cuda device: {torch.cuda.current_device()}")
 
-    # Experiment
-    exp = Experiment(image_dir, label_path, pred_dir, stim_type, top1, data_path, class_num, check_time_usage, env_type, model_type, episode_num, print_interval, learning_rate, discount, average_window, device)
+    # Perform experiment
+    exp = Experiment(image_dir, label_path, pred_dir, stim_type, top1, data_path, class_num, env_type, model_type, episode_num, print_interval, learning_rate, gamma, batch_size, render, device, argv)
     model, train_returns, test_returns = exp.train()
 
     # Save the information
     if cfg.save_info:
         os.makedirs(f"{cfg.directory.output_dir}")
-        torch.save(model.state_dict(), os.path.join(cfg.directory.output_dir, f'{model_type}.pth'))
-        train_returns_df = pd.DataFrame.from_dict(train_returns)
-        test_returns_df = pd.DataFrame.from_dict(test_returns)
-        train_returns_df.to_csv(os.path.join(cfg.directory.output_dir, f'TrainReturns_{model_type}.csv'))
-        test_returns_df.to_csv(os.path.join(cfg.directory.output_dir, f'TestReturns_{model_type}.csv'))
-
+        torch.save(model.state_dict(), os.path.join(cfg.directory.output_dir, f'{env_type}_{model_type}.pth'))
+        train_returns_df = pd.DataFrame.from_dict(dict([(k, pd.Series(v, dtype='float64')) for k, v in train_returns.items()]))
+        test_returns_df = pd.DataFrame.from_dict(dict([(k, pd.Series(v, dtype='float64')) for k, v in test_returns.items()]))
+        train_returns_df.to_csv(os.path.join(cfg.directory.output_dir, f'TrainReturns_{env_type}_{model_type}.csv'))
+        test_returns_df.to_csv(os.path.join(cfg.directory.output_dir, f'TestReturns_{env_type}_{model_type}.csv'))
 if __name__ == '__main__':
     main()
