742ee3607705bb253b2c7b1e5d26b925046a08e3
namin-an <namin0202@gmail.com>
2022-10-31 16:39:46 +0900

    Add time and memory

--- code/main.py
+++ code/main.py
@@ -4,6 +4,8 @@ from omegaconf import DictConfig
 import numpy as np
 import pandas as pd
 import torch
+import time
+import tracemalloc
 
 import sys
 sys.path.append(os.getcwd())
@@ -37,11 +39,11 @@ def main(cfg: DictConfig):
 
     env_type = cfg.environment.name
     model_type = cfg.algorithm.name
-    episode_num = cfg.training.episode_num
+    episode_num = cfg.environment.episode_num
     print_interval = cfg.training.print_interval
     learning_rate = cfg.reinforcement.learning_rate
     gamma = cfg.reinforcement.gamma
-    batch_size = cfg.training.batch_size
+    batch_size = cfg.environment.batch_size
     render = cfg.training.render
 
     # Algorithm specificity
@@ -58,7 +60,15 @@ def main(cfg: DictConfig):
 
     # Perform experiment
     exp = Experiment(image_dir, label_path, pred_dir, stim_type, top1, data_path, class_num, env_type, model_type, episode_num, print_interval, learning_rate, gamma, batch_size, render, device, argv)
-    model, train_returns, time_memory = exp.train()
+    if cfg.monitor_tm:
+        start_time = time.time()
+        tracemalloc.start()
+    model, train_returns = exp.train()
+    if cfg.monitor_tm:
+        memory = tracemalloc.get_traced_memory()
+        print(f"Total training time: {time.time() - start_time} (seconds) \n Memory (current & peak): {memory} [b]")
+        print(f"Total training time: {(time.time() - start_time) / 60} (minutes) \n Memory (peak): {memory[-1] / (1e6)} [mb]")
+        tracemalloc.stop()
 
     # Save the information
     if cfg.save_info:
@@ -66,8 +76,6 @@ def main(cfg: DictConfig):
         torch.save(model.state_dict(), os.path.join(cfg.directory.output_dir, f'{env_type}_{model_type}.pth'))
         train_returns_df = pd.DataFrame.from_dict(dict([(k, pd.Series(v, dtype='float64')) for k, v in train_returns.items()]))
         train_returns_df.to_csv(os.path.join(cfg.directory.output_dir, f'TrainReturns_{env_type}_{model_type}.csv'))
-        time_memory_df = pd.DataFrame.from_dict(dict([(k, pd.Series(v, dtype='float64')) for k, v in time_memory.items()]))
-        time_memory_df.to_csv(os.path.join(cfg.directory.output_dir, f'TimeMemory_{env_type}_{model_type}.csv'))
 
 if __name__ == '__main__':
     main()
