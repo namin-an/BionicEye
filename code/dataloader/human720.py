536028c4750697750909aa2ac6ee963f25c3a9e4
namin-an <namin0202@gmail.com>
2022-11-01 14:16:50 +0900

    Delete unnecessary variables

--- code/dataloader/human720.py
+++ code/dataloader/human720.py
@@ -10,26 +10,16 @@ import cv2 as cv
 class HumanDataLoader():
     def __init__(self, image_dir, label_path, pred_dir, stim_type, top1):
 
-        """
-        image_dir: '/Users/naminan/Development/Project/code/data/sample_for_dev_test'
-        label_path: '/Users/naminan/Development/Project/code/data/211105_QAs_for_Set0_CNN_SVC_4classes_full.csv'
-        pred_dir: '/Users/naminan/Development/Project/code/data/Human_Expert/211202'
-        """
-
         self.image_dir = image_dir
-        self.label_path = label_path
-        self.pred_dir = pred_dir
-        self.stim_type = stim_type
-        self.top1 = top1
         
         # FOR THE LABELS
-        self.question_df = pd.read_csv(self.label_path)
+        self.question_df = pd.read_csv(label_path)
         self.labels = list(self.question_df['Answer'].values)
 
         self.main_files = glob(pred_dir + '/main_test*.csv')
-        if self.stim_type == 'opt':
+        if stim_type == 'opt':
             self.sel_ppl = list(range(300, 309)) + list(range(400, 408)) + [611] # 18 subjects
-        elif self.stim_type == 'elec':
+        elif stim_type == 'elec':
             self.sel_ppl = [499, 500, 502] + list(range(503, 509)) + list(range(602, 607)) + list(range(608, 612)) # 18 subjects
        
         # FOR THE HUMAN PREDICTIONS
@@ -37,7 +27,7 @@ class HumanDataLoader():
         for m in tqdm(range(len(self.main_files))):
             temp_df = pd.read_csv(self.main_files[m])
             temp_df = temp_df[temp_df['유저식별아이디'].isin(self.sel_ppl)]
-            if self.top1:
+            if top1:
                 temp_df = temp_df.loc[:, temp_df.columns.str.startswith('선택_A')]
             else:
                 temp_df = temp_df.loc[:, temp_df.columns.str.startswith('선택_B')]
