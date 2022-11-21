20d33af46f9dc9a9ed4c03d143a195c0679f72c3
namin-an <namin0202@gmail.com>
2022-11-21 10:51:21 +0900

    Delete REINFORCE

--- code/main.py
+++ code/main.py
@@ -70,8 +70,8 @@ def main(cfg: DictConfig):
         if cfg.monitor_tm:
             start_time = time.time()
             tracemalloc.start()
-            if env_type == 'Bioniceye':
-                exp.pretrain()
+            # if env_type == 'Bioniceye':
+            #     exp.pretrain()
             train_returns, correctness = exp.train()
             memory = tracemalloc.get_traced_memory()
             print(f"Total training time: {time.time() - start_time : .2f} (seconds) or  {(time.time() - start_time) / 3600 : .2f} (hours)")
